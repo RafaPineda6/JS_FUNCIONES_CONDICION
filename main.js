3aecbc3719a13215b0a196a7dfea9b0760787de8
rafael pineda <=>
2020-11-25 21:26:08 -0500

    Condiciones

---
+++ main.js
@@ -0,0 +1,162 @@
+//let edad = prompt("ingrese su edad");
+
+//if(edad >= 18){
+//    alert("Usted es mayor de edad");
+//}else{
+//    alert("Usted es menor de edad");
+//}
+
+
+
+
+//let tarjeta = true
+
+//if(tarjeta != true){
+//    alert("entro por la puerta");
+//}else{
+//    alert("no puede entrar por la puerta");
+//}
+
+
+
+
+//let = "23";
+
+//if(numero == 23){
+//    alert("entro el numero");
+//}else{
+//    alert("el numero no entro")
+//}
+
+
+
+
+//let tarjeta = true
+
+//if(tarjeta !== false){
+//    alert("entro por la puerta");
+//}else{
+//    alert("no puede entrar por la puerta");
+//}
+
+
+
+
+//let num = prompt("Ingrese un numero mayor que (10)");
+
+//if(num > 10){
+//    alert("El numero ingresado es : "+num+" y es mayor a 10, num");
+//}else{
+//    alert("El numero no es el correcto");
+//}
+
+
+
+
+//let num = prompt("Ingrese un numero mayor que (10)");
+
+//if(num >= 10){
+//    alert("El numero ingresado es : "+num+" y es mayoro igual a 10, num");
+//}else{
+//    alert("El numero no es el correcto");
+//}
+
+
+
+
+
+//let num = prompt("Ingrese un numero mayor que (10)");
+
+//if(num < 10){
+//    alert("El numero ingresado es : "+num+" y es menor que 10, num");
+//}else{
+//    alert("El numero no es el correcto");
+//}
+
+
+
+
+//let num = prompt("Ingrese un numero mayor que (10)");
+
+//if(num <= 10){
+//    alert("El numero ingresado es : "+num+" y es menor o igual que 10, num");
+//}else{
+//    alert("El numero no es el correcto");
+//}
+
+
+
+//  Operación condicion AR
+//let a = false;
+//let b = false;
+
+//if(a &= b){
+//   alert(true);
+//}else{
+//   alert(false)
+//}
+
+
+
+// Operadores condición OR
+//let a = false;
+//let b = false;
+
+//if(a |= b){
+//    alert(true);
+//}else{
+//    alert(false)
+//}
+
+
+
+
+// Operacion condicion XOR
+//let a = true;
+//let b = false;
+
+//if(a ^= b){
+//    alert(true);
+//}else{
+//    alert(false)
+//}
+
+
+
+//let edad = prompt("Ingrese su edad")
+
+//if(edad == 18){ //si
+//    alert("Cumplio los 18 y es mayor de edad");
+//}else if{(edad > 18){si no
+//   alert("es mayor de edad");
+//}else{ //no
+//    alert("es menor de edad");
+//}
+
+
+
+
+//let edad = prompt("Ingrese su edad")
+
+//if(edad == 18){ //si
+//    alert("Cumplio los 18 y es mayor de edad");
+//}else if ( (edad >= 19) && (edad <= 20) ){// si no
+//    alert( ("es mayor de edad y es menor a 20") );
+//}else if ( (edad >= 21) && (edad <= 25) ){// si no
+//        alert("es mayor de edad y es menor a 25");
+//}else{ //no
+//    alert("es menor de edad");
+//}
+  
+
+
+
+function Rafael(Plata){
+    let tendero=Plata;
+    let Vueltos;
+
+    Vueltos = tendero-1500;
+    return Vueltos;
+}
+
+alert("Mis Vueltos son: "+miguel(2000));
